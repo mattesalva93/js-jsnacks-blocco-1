73636b9d61df642b3b9eb22a9714071c42da18bd
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-16 16:36:46 +0100

    JSnack-2 done

--- JSnack-1/myscript.js
+++ JSnack-1/myscript.js
@@ -6,14 +6,15 @@ quando la somma degli elementi è minore di 50. */
 //creo un array vuoto
 let numberList = [];
 
-//chiedo all'utente di inserire un numero tramite prompt
+//chiedo all'utente di inserire un numero tramite prompt, lo inserisco nell'array e lo sommo;
 let sum = 0;
 do{
     let userInput = parseInt(prompt("Inserisci un numero!"))
     numberList.push(userInput);
     sum += userInput;
-    
+//ne esco quando ho raggiunto somma 50 
 }while(sum <= 50);
 
+//stampo per visione la mia array e la mia somma finale
 document.writeln("Eccola tua lista di numeri!" + numberList);
 document.writeln("Eccola tua somma!" + sum);
--- JSnack-2/myscript.js
+++ JSnack-2/myscript.js
@@ -2,3 +2,17 @@
 Fai inserire un numero, che chiameremo N, all’utente.
 Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
 Ogni volta che ne crei uno, stampalo a schermo. */
+
+//richiedo all'utente un numero
+let userInput = parseInt(prompt("Inserisci un numero!"));
+console.log(userInput);
+
+for (i=0; i < userInput; i++)
+{
+    let userArray = [];
+    for(k=0; k < 10; k++)
+    {
+        userArray.push(Math.floor(Math.random()*100+1));
+    }
+    document.writeln(userArray);
+}
